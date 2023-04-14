66fe66589e5d94871c8629d9e063d3c43567ce49
dhyey870 <dhyey870@gmail.com>
2023-04-14 15:15:39 -0700

    Final Commit

--- index.js
+++ index.js
@@ -7,22 +7,14 @@ const methodOverride = require("method-override");
 const dataModel = require("./models/dataModel");
 const dotenv = require("dotenv").config();
 
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.vmsu9lg.mongodb.net/test`
-  )
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
+//set view engine as ejs and set dynamic path for view folder in which all ejs files are there
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+//use middleware for express and set public directory for static files such as css, js
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+//to override form submit method from POST to PUT
 app.use(methodOverride("_method"));
 
 let options = {
@@ -31,10 +23,16 @@ let options = {
 
 let geoCoder = nodeGeocoder(options);
 
+/**
+ * home route - "/"
+ **/
+
 app.get("/", async (req, res) => {
   const data = await dataModel.findOne({});
+
+  //if there is no data inside database then it will create default one
   if (data) {
-    res.render("index.ejs", { data, locations: data.locations });
+    res.render("app.ejs", { data, locations: data.locations });
   } else {
     var newData = new dataModel({
       totalCount: 0,
@@ -43,7 +41,7 @@ app.get("/", async (req, res) => {
     newData
       .save()
       .then((d) => {
-        res.render("index.ejs", { data: d, locations: d.locations });
+        res.render("app.ejs", { data: d, locations: d.locations });
       })
       .catch((err) => {
         console.log(err);
@@ -51,13 +49,18 @@ app.get("/", async (req, res) => {
   }
 });
 
+/**
+ * Put route to update model in database - "/increament"
+ */
+
 app.put("/increament", async (req, res) => {
   const oldData = await dataModel.findOne({});
 
+  //use geocoder to convert lat, long into location
   geoCoder
     .reverse({ lat: req.body.lat, lon: req.body.long })
     .then((response) => {
-      // console.log(response[0]);
+      //first check if specific city already there in database then it just update it's count
       var flag = false;
       for (let loc of oldData.locations) {
         if (
@@ -74,7 +77,6 @@ app.put("/increament", async (req, res) => {
           country: response[0].country,
           clicks: 1,
         });
-        console.log(oldData.locations);
       }
 
       oldData.totalCount += 1;
@@ -83,18 +85,30 @@ app.put("/increament", async (req, res) => {
         .save()
         .then((d) => {
           console.log("success");
+          res.redirect("/");
         })
         .catch((err) => {
           console.log(err);
         });
-
-      res.redirect("/");
     })
     .catch((e) => {
       console.log(e);
     });
 });
 
+//connecting mongoose with mongoDB
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.vmsu9lg.mongodb.net/test`
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// use process enviornment port to start HTTP server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server Started On Port No ${port}`);
